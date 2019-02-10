00063e03af69c8d6953ae37d2930b398d4c523fd
goneko <starquality131@gmail.com>
2019-02-10 21:56:32 +0800

    add build files

---
+++ build/precache-manifest.a67a1cdcc23e3818aacef065c773b105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "47ade80eb6010ce58725",
+    "url": "/static/js/main.47ade80e.chunk.js"
+  },
+  {
+    "revision": "00eb4347c34bb2fc2220",
+    "url": "/static/js/1.00eb4347.chunk.js"
+  },
+  {
+    "revision": "47ade80eb6010ce58725",
+    "url": "/static/css/main.0c48a9f9.chunk.css"
+  },
+  {
+    "revision": "00eb4347c34bb2fc2220",
+    "url": "/static/css/1.6545caa2.chunk.css"
+  },
+  {
+    "revision": "5443102352fd1854908402c41d0dc47a",
+    "url": "/index.html"
+  }
+];
