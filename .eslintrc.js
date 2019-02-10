6e32bcc244d5fc63b3a68ae7e7cee1ebdcc778f1
goneko <starquality131@gmail.com>
2019-02-10 22:40:32 +0800

    init again

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "rules": {
+        "quotes": ["error", "single"],
+        "react/prefer-stateless-function": 0,
+        "indent": ["error", 4]
+    }
+};
---
+++ src/components/SelectRow.js
@@ -0,0 +1,97 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-filename-extension */
+import React, { PureComponent, Fragment } from 'react';
+import '../css/SelectRow.scss';
+import base from '../base/base';
+
+class SelectRow extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usePercentage: '',
+            // 使用的油品g數
+            useOilGram: '',
+            // 需要的ＮaOHg數
+            NaOHGram: '',
+        };
+    }
+
+    doOilOptions = (data) => (
+
+        data.map((oil, oilOptionIndex) => (
+
+            <option 
+                value={oil.enName} 
+                key={oilOptionIndex}
+                data-index={oilOptionIndex}
+            >
+                {oil.name}
+            </option>
+
+        ))
+
+    )
+
+    calculation = () => {}
+
+    render() {
+
+        return (
+            <ul className="tableRow">
+                <li className="tableCol oil_select">
+                    <select 
+                        id="oilName"
+                        onChange={this.props.handleOilSelect}
+                        value={this.props.defaultValue}
+                        data-row-index={this.props.index}
+                    >
+                        {(this.props.defaultValue === '') ? (
+                            <option value="">請選擇</option>
+                        ) : null}
+                        {this.doOilOptions(this.props.oils)}
+                    </select>
+                    {(this.props.defaultValue !== '') ? (
+                        <Fragment>
+                            &nbsp;<span>({this.props.data.oilData.suggestPercentage})</span>
+                        </Fragment>
+                    ) : null}
+                </li>
+                {/*INS*/}
+                <li className="tableCol">{this.props.data.oilData.INS}</li>
+                {/*皂化價*/}
+                <li className="tableCol">{this.props.data.oilData.saponificationValue}</li>
+                {/*oil g*/}
+                <li className="tableCol">
+                    {base.strip(this.props.totalOilGram * this.props.data.oilPercentage / 100)}
+                </li>
+                {/*NaOH g*/}
+                <li className="tableCol">
+                    {base.strip(this.props.totalOilGram * this.props.data.oilPercentage / 100 * this.props.data.oilData.saponificationValue)}
+                </li>
+                {/*oil %*/}
+                <li className="tableCol oil_percentage">
+                    <input
+                        type="number"
+                        data-row-index={this.props.index}
+                        onChange={this.props.handleOilPercentageChange}
+                    />&nbsp;%
+                </li>
+                {/*Calc. INS*/}
+                <li className="tableCol">
+                    {base.strip(this.props.data.oilData.INS * this.props.data.oilPercentage / 100)}
+                </li>
+                {/*操作*/}
+                <li className="tableCol">
+                    <a 
+                        className="delete_btn"
+                        data-row-index={this.props.index}
+                        href="javascript:;"
+                        onClick={this.props.handleDeleteRow}
+                    >-</a>
+                </li>
+            </ul>
+        );
+    }
+}
+
+export default SelectRow;
