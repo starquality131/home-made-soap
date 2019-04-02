4c4b64fc02c83df6fe5d2e7782de5d45836ead04
goneko <starquality131@gmail.com>
2019-04-03 00:37:23 +0800

    Updates

---
+++ precache-manifest.f7d4a12288b64fa438378981c065941f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24e4ccd095d372041889",
+    "url": "/home-made-soap/static/js/runtime~main.24e4ccd0.js"
+  },
+  {
+    "revision": "d8f17c326ff2ed758f74",
+    "url": "/home-made-soap/static/js/main.d8f17c32.chunk.js"
+  },
+  {
+    "revision": "1722a05669464a827d6a",
+    "url": "/home-made-soap/static/js/1.1722a056.chunk.js"
+  },
+  {
+    "revision": "d8f17c326ff2ed758f74",
+    "url": "/home-made-soap/static/css/main.6505bd40.chunk.css"
+  },
+  {
+    "revision": "1722a05669464a827d6a",
+    "url": "/home-made-soap/static/css/1.6545caa2.chunk.css"
+  },
+  {
+    "revision": "02674cc259749ea0b40d68e62db77cb8",
+    "url": "/home-made-soap/index.html"
+  }
+];
--- src/base/base.js
+++
@@ -1,6 +0,0 @@
-export default {
-
-    strip: (num, precision = 12) => +parseFloat(num.toPrecision(precision)),
-    calc: (num, fixed = 0) => +parseFloat(num.toFixed(fixed)),
-
-};
